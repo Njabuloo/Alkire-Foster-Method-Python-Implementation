cd7dca1dad402cd7248ded5e9418d0eef45650c7
Njabulo Masilela <masilelanjabulo01@gmail.com>
2023-10-15 15:49:30 +0200

    calculate_subgroup_data for plotting

--- AF_calculations.py
+++ AF_calculations.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from typing import List
 
-def get_deprivation_matrix(dimensions_matrix: pd.DataFrame, cutoff_matrix: pd.DataFrame) -> pd.DataFrame:
+
+def get_deprivation_matrix(
+    dimensions_matrix: pd.DataFrame, cutoff_matrix: pd.DataFrame
+) -> pd.DataFrame:
     """
     Calculate the deprivation matrix (g0) based on dimensions and cutoff values.
 
@@ -20,13 +23,17 @@ def get_deprivation_matrix(dimensions_matrix: pd.DataFrame, cutoff_matrix: pd.Da
 
     # Generate values for the deprivation matrix based on dimensions and cutoff values
     for index, col_name in enumerate(dimensions_column_names):
-        cutoff_value = cutoff_matrix.at[index, 'Cutoff']
-        deprivation_matrix[col_name] = deprivation_matrix[col_name].apply(lambda value: 1 if value == cutoff_value else 0)
+        cutoff_value = cutoff_matrix.at[index, "Cutoff"]
+        deprivation_matrix[col_name] = deprivation_matrix[col_name].apply(
+            lambda value: 1 if value == cutoff_value else 0
+        )
 
     return deprivation_matrix
 
 
-def calculate_weighted_deprivation_matrix(deprivation_matrix: pd.DataFrame, weights_matrix: pd.DataFrame) -> pd.DataFrame:
+def calculate_weighted_deprivation_matrix(
+    deprivation_matrix: pd.DataFrame, weights_matrix: pd.DataFrame
+) -> pd.DataFrame:
     """
     Calculate the weighted deprivation matrix based on the deprivation matrix and weights.
 
@@ -40,15 +47,17 @@ def calculate_weighted_deprivation_matrix(deprivation_matrix: pd.DataFrame, weig
     # Iterate through columns of the deprivation matrix
     for index, col_name in enumerate(deprivation_matrix.columns):
         # Get the weight corresponding to the column
-        weight = weights_matrix.at[index, 'Weight']
-        
+        weight = weights_matrix.at[index, "Weight"]
+
         # Multiply the values in the column by the weight
         deprivation_matrix[col_name] = deprivation_matrix[col_name] * weight
-    
+
     return deprivation_matrix
 
 
-def calculate_deprevation_scores(weighted_deprivation_matrix: pd.DataFrame) -> pd.DataFrame:
+def calculate_deprevation_scores(
+    weighted_deprivation_matrix: pd.DataFrame,
+) -> pd.DataFrame:
     """
     Calculate deprivation scores based on the weighted deprivation matrix.
 
@@ -62,20 +71,22 @@ def calculate_deprevation_scores(weighted_deprivation_matrix: pd.DataFrame) -> p
     number_of_rows = len(weighted_deprivation_matrix)
 
     # Initialize a DataFrame to store the deprivation scores
-    data = {'Scores': [0] * number_of_rows}
+    data = {"Scores": [0] * number_of_rows}
     deprevation_scores = pd.DataFrame(data)
 
     # Calculate the deprivation score for each row in the weighted deprivation matrix
     for index in range(number_of_rows):
         deprevation_score = weighted_deprivation_matrix.iloc[index].sum()
-        
+
         # Update the 'Scores' column in the deprivation scores DataFrame
-        deprevation_scores.at[index, 'Scores'] = deprevation_score
+        deprevation_scores.at[index, "Scores"] = deprevation_score
 
     return deprevation_scores
 
 
-def get_censored_vector(deprevation_scores_vector: pd.DataFrame, cutoff_score: int) -> pd.DataFrame:
+def get_censored_vector(
+    deprevation_scores_vector: pd.DataFrame, cutoff_score: int
+) -> pd.DataFrame:
     """
     Generate a censored vector based on deprivation scores and a cutoff score.
 
@@ -91,10 +102,12 @@ def get_censored_vector(deprevation_scores_vector: pd.DataFrame, cutoff_score: i
 
     # Iterate through the rows of the vector and apply censoring
     for index in range(len(censored_vector)):
-        deprivation_score = censored_vector.at[index, 'Scores']
+        deprivation_score = censored_vector.at[index, "Scores"]
 
         # Apply censoring: If the deprivation score is below the cutoff, set it to 0
-        censored_vector.at[index, 'Scores'] = deprivation_score if deprivation_score >= cutoff_score else 0
+        censored_vector.at[index, "Scores"] = (
+            deprivation_score if deprivation_score >= cutoff_score else 0
+        )
 
     return censored_vector
 
@@ -123,7 +136,9 @@ def get_head_count_ratio(censored_vector: pd.DataFrame) -> float:
     return headcount_ratio
 
 
-def get_average_deprivation_score(censored_vector: pd.DataFrame, number_of_dimensions: int) -> float:
+def get_average_deprivation_score(
+    censored_vector: pd.DataFrame, number_of_dimensions: int
+) -> float:
     """
     Calculate the average deprivation score for the Alkire-Foster method.
 
@@ -137,7 +152,7 @@ def get_average_deprivation_score(censored_vector: pd.DataFrame, number_of_dimen
         float: The average deprivation score as a percentage.
     """
     # Calculate the sum of deprivation scores for individuals classified as poor
-    scores_sum = censored_vector['Scores'].sum()
+    scores_sum = censored_vector["Scores"].sum()
 
     # Calculate the total count of poor individuals
     total_poor_people = len(censored_vector.loc[censored_vector["Scores"] > 0])
@@ -151,7 +166,9 @@ def get_average_deprivation_score(censored_vector: pd.DataFrame, number_of_dimen
     return average_deprivation_score_percentage
 
 
-def get_adjusted_head_count_ratio(average_deprivation_score: float, headcount_ratio: float) -> float:
+def get_adjusted_head_count_ratio(
+    average_deprivation_score: float, headcount_ratio: float
+) -> float:
     """
     Calculate the adjusted headcount ratio for the Alkire-Foster method.
 
@@ -172,3 +189,37 @@ def get_adjusted_head_count_ratio(average_deprivation_score: float, headcount_ra
     adjusted_headcount_ratio = 100 * headcount_ratio * average_deprivation_score
 
     return adjusted_headcount_ratio
+
+
+def calculate_subgroup_data(
+    demographics_matrix: pd.DataFrame, censored: pd.DataFrame
+) -> list:
+    """
+    Calculate subgroup data based on the demographics matrix and censored data.
+
+    Args:
+        demographics_matrix (pd.DataFrame): DataFrame containing demographic information.
+        censored (pd.DataFrame): DataFrame containing censored data.
+
+    Returns:
+        list: List of subgroups along with their corresponding data.
+    """
+    sub_groups_data = []
+
+    for col in demographics_matrix.columns:
+        sub_group_name = col
+        sub_group_data = {}
+        total_censored = len(censored[censored["Scores"] > 0])
+
+        for value in demographics_matrix[col].unique():
+            subgroup_censored_count = len(
+                demographics_matrix[
+                    (demographics_matrix[col] == value) & (censored["Scores"] > 0)
+                ]
+            )
+            percentage = subgroup_censored_count / total_censored
+            sub_group_data[value] = percentage
+
+        sub_groups_data.append([sub_group_name, sub_group_data])
+
+    return sub_groups_data
